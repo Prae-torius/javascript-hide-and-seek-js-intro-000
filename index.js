5ecf48bb69048c7f6a72ccb8c6b6a2b35f2c9714
Preston A Arnette <arnettepreston@gmail.com>
2018-08-22 19:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const list = document.querySelectorAll('ul.ranked-list li')
+  const list = document.querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < list.length; i++) {
     return list[i].innerHTML += n; 
   }
